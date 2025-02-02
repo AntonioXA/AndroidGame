2102b5f578e83e30e661e3fb1bba662cfc3a9217
AntonioXA <antoniomf.fp@gmail.com>
2025-02-02 19:22:10 +0100

    Segundo commit, ahora sin archivos sensibles

---
+++ android/build/intermediates/assets/debug/mergeDebugAssets/tilesetprototype_v3.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="tilesetprototype_v3" tilewidth="32" tileheight="32" tilecount="551" columns="29">
+ <image source="Tilesets/tilesetprototype_v3.png" width="928" height="608"/>
+</tileset>
